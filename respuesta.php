0b69636fc3b1da0898b4b1938b443d83f8ab9498
Wilson <wilsonrueda2@gmail.com>
2020-09-11 19:52:54 -0500

    se corrigio la condicional que evalua la edad

--- respuesta.php
+++ respuesta.php
@@ -9,7 +9,26 @@
 <body>
     
 <?php
-            if($_POST["Edad"]>=18){
+            if($_POST["Edad"]>60){
+                echo "Usted es un adulto mayor <hr>";
+                echo "Usted es mayor de edad y estos son los detalles de su formulario <hr>";
+                $nombres = $_POST["Nombres"];
+                    echo "Tu nombre es $nombres<hr>";
+
+                $apellidos = $_POST["Apellidos"];
+                    echo "Tu apellido es $apellidos <hr>";
+
+                $identificacion = $_POST["Cedula"];
+                    echo "Tu numero de identificacion es $identificacion <hr>";
+
+                $correo = $_POST["Correo"];
+                    echo "Tu correo es $correo <hr>";
+
+                $edad = $_POST["Edad"];
+                    echo "Tu edad es $edad <hr>";
+                    echo "Tu comentario se registro correctamente";
+            }
+            elseif($_POST["Edad"]>=18){
 
                     echo "Usted es mayor de edad y estos son los detalles de su formulario <hr>";
                 $nombres = $_POST["Nombres"];
@@ -28,7 +47,8 @@
                     echo "Tu edad es $edad <hr>";
                     echo "Tu comentario se registro correctamente"; 
         
-            }else{
+            }
+            else{
                 echo "Usted es menor de Edad, por lo cual no tiene permitido diligenciar este
                 formulario";
             }
